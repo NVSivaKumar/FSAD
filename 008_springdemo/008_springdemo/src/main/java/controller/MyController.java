0d248c5bde445a64770a83babe56d79393d82cdc
NVSivaKumar <2400032513@kluniversity.in>
2026-03-21 09:55:03 +0530

    Add files via upload

---
+++ 008_springdemo/008_springdemo/src/main/java/controller/MyController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class MyController {
+	@RequestMapping("/")
+	public String welcome(Model M)
+	{
+		M.addAttribute("msg", "Hello world from Spring MVC!");
+		return "hello.jsp";
+	}
+}
+
